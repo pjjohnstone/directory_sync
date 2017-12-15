98fe52cc5538e728c4f9a8ebb4019611fcf84f60
Patrick Johnstone <johnstonep@gmail.com>
2017-12-15 10:35:34 +0000

    Checksumming now optional, default to comparing mtime

--- resources/copy.rb
+++ resources/copy.rb
@@ -3,10 +3,12 @@ require 'pathname'
 
 property :target, String, name_property: true
 property :source, String
+property :checksum, [true, false], default: false
 
 action :copy do
   source = new_resource.source
   target = new_resource.target
+  checksum = new_resource.checksum
   files = []
   Find.find(source) { |f| files.push(win_friendly_path(f)) }
   files.each do |file|
@@ -20,8 +22,8 @@ action :copy do
     else
       source_item = ::File.directory?(source) ? (Pathname.new(::File.path(file)).relative_path_from Pathname.new(source)) : (Pathname.new(::File.path(file)).relative_path_from Pathname.new(::File.dirname(source)))
       target_path = win_friendly_path(::File.join(target, source_item))
-      if file_checksum_match?(file, target_path)
-        Chef::Log.debug("#{file} and #{target_path} checksums match, skipping...")
+      if files_match?(file, target_path, checksum)
+        Chef::Log.debug("#{file} and #{target_path} match, skipping...")
       else
         converge_by "Copying #{file} to #{target_path}..." do
           file_copy(file, target_path)
@@ -40,8 +42,15 @@ def file_copy(source, target)
   ::FileUtils.cp source, target
 end
 
+def files_match?(source, target, checksum)
+  return false unless ::File.exist?(target)
+  checksum ? file_checksum_match?(source, target) : file_modified_date_match?(source, target)
+end
+
+def file_modified_date_match?(source, target)
+  ::File.mtime(source) == ::File.mtime(target)
+end
+
 def file_checksum_match?(source, target)
-  if ::File.exist?(target)
-    ::Digest::SHA256.file(source).hexdigest == ::Digest::SHA256.file(target).hexdigest
-  end
+  ::Digest::SHA256.file(source).hexdigest == ::Digest::SHA256.file(target).hexdigest
 end
